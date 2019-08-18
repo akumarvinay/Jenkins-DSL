f30c80f160a831daaff970e04bbb11911792d5fc
Vinay Kumar <akumarvinay@gmail.com>
2019-08-18 16:14:09 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -26,10 +26,10 @@ def call(body)
 			withSonarQubeEnv('SONAR_POC') 
 			{ // If you have configured more than one global server connection, you can specify its name
       			sh """
-				  ${SONAR_TOOL}/bin/sonar-scanner -Dsonar.projectKey=myproject 
-				  -Dsonar.sources=src -Dsonar.projectName=myfirstApp
-				  -Dsonar.projectVersion=1.0
-				  -Dsonar.sources=src
+				  ${SONAR_TOOL}/bin/sonar-scanner -Dsonar.projectKey=myproject /
+				  -Dsonar.sources=src -Dsonar.projectName=myfirstApp /
+				  -Dsonar.projectVersion=1.0 /
+				  -Dsonar.sources=src /
 				  -Dsonar.language=java
 			"""
     			}
